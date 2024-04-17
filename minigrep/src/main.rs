0062aee4ff0c287a3acd1e5e86a07ecf4195215d
obf1313 <chenna2@sisyphe.com.cn>
2024-04-17 13:42:00 +0800

    feat: 重定向错误信息的输出

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -9,18 +9,20 @@ fn main() {
     // unwrap_or_else 是定义在 Result<T,E> 上的常用方法，如果 Result 是 Ok，那该方法就类似 unwrap：返回 Ok 内部的值
     // 如果是 Err，就调用闭包中的自定义代码对错误进行进一步处理
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        // 将错误信息重定向到 stderr 很简单
+        eprint!("Problem parsing arguments: {err}");
         // 当 Result 包含错误时，我们不再调用 panic 让程序崩溃，而是通过 process::exit(1) 来终结进程
         process::exit(1);
         // 不太懂怎么在报错
     });
     // dbg! 宏来输出读取到的数组内容
     // dbg!(args);
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
+    eprint!("Searching for {}", config.query);
+    eprint!("In file {}", config.file_path);
     // run(config);
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprint!("Application error: {e}");
         process::exit(1);
     }
 }
+// cargo run -- to poem.txt > output.txt
